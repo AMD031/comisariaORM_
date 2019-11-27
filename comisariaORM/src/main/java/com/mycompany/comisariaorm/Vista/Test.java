e9ffd2f606799636e28a73463808b6f14921a1ab
Antonio .M.D <teclado1333@gmail.com>
2019-11-27 08:28:42 +0100

    iniciando proyecto

---
+++ comisariaORM/src/main/java/com/mycompany/comisariaorm/Vista/Test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.comisariaorm.Vista;
+
+/**
+ *
+ * @author Your Name <Antonio Martinez Diaz>
+ */
+public class Test {
+   
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
